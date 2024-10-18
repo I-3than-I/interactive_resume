b6aae18a1c80c1dce74044bbb07850ca52b95bd5
Sneha <155668049+sirisneha99@users.noreply.github.com>
2024-10-18 15:17:33 -0400

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import logo from "../assets/logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/duck-johnson" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/duck-johnson" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://instagram.com/duck_johnson" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com/duck_johnson" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
